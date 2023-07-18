e49f6729d0cb3a839161af13074565131893746c
Hideki Yamamoto <hidei.1516.web@gmail.com>
2023-07-19 00:21:37 +0900

    タスクを関数で記述する方法に修正

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,27 @@
-// gulpプラグインの読み込み
-const gulp = require('gulp');
+const gulp = require('gulp'); // gulpプラグインの読み込み
+const sass = require('gulp-sass')(require('sass')); // Sassをコンパイルするプラグインの読み込み
+const browserSync = require('browser-sync'); // browser-syncプラグインの読み込み
 
-// Sassをコンパイルするプラグインの読み込み
-const sass = require('gulp-sass')(require('sass'));
+const sassCompile = (done) => {
+  gulp.src('./src/scss/*.scss') // src/scssのscssファイルを取得
+    .pipe(sass.sync({ // sassのコンパイルを実行
+      outputStyle: 'expanded' // コンパイル後の整形
+    }))
+    .on('error', sass.logError) // sassのコンパイルエラーを表示
+    .pipe(gulp.dest('./dist/css')); // コンパイル後の出力先を指定
+  done();
+};
 
-// タスクの作成
-gulp.task('default', () => {
-  return gulp.watch('src/scss/style.scss', () => { // style.scssが更新された場合の処理
-    return (
-      gulp
-        .src('src/scss/style.scss') // style.scssを取得
-        .pipe(sass({ // sassのコンパイルを実行
-          outputStyle: 'expanded', // コンパイル後の整形
-        })
-        .on('error', sass.logError) // sassのコンパイルエラーを表示
-        )
-        .pipe(gulp.dest('dist/css')) // cssフォルダーに保存
-    );
-  });
-});
+// タスク化
+exports.sassCompile = sassCompile; // sassのコンパイル処理を記述した変数をタスク化
+
+// 監視ファイル（変更のたびにタスクを実行）
+const watchFile = (done) => {
+  gulp.watch('./src/scss/*.scss', sassCompile); // 第1引数：監視するファイルのパス　第2引数：コンパイル処理の変数
+  done();
+};
+
+// タスク実行
+exports.default = gulp.series(
+  watchFile, sassCompile
+);

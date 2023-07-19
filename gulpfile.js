6c8060b98bfc63cc19cbe9e9e507b6edf371b09b
Hideki Yamamoto <hidei.1516.web@gmail.com>
2023-07-20 01:56:08 +0900

    browser-syncを導入

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const gulp = require('gulp'); // gulpプラグインの読み込み
 const sass = require('gulp-sass')(require('sass')); // Sassをコンパイルするプラグインの読み込み
+const plumber = require('gulp-plumber'); // コンパイル時にエラーになっても中断しない
 const browserSync = require('browser-sync'); // browser-syncプラグインの読み込み
 
 const sassCompile = (done) => {
   gulp.src('./src/scss/*.scss') // src/scssのscssファイルを取得
+    .pipe(plumber())
     .pipe(sass.sync({ // sassのコンパイルを実行
       outputStyle: 'expanded' // コンパイル後の整形
     }))
@@ -12,16 +14,39 @@ const sassCompile = (done) => {
   done();
 };
 
+// リロードするhtml
+const reloadFile = (done) => {
+  browserSync.init({
+    server : {
+      baseDir : './',
+      index : 'index.html',
+      directory: true,
+    }
+  });
+  done();
+};
+
+// リロード設定
+const reloadBrowser = (done) => {
+  browserSync.reload();
+  done();
+};
+
 // タスク化
 exports.sassCompile = sassCompile; // sassのコンパイル処理を記述した変数をタスク化
+exports.reloadFile = reloadFile;
+exports.reloadBrowser = reloadBrowser;
 
 // 監視ファイル（変更のたびにタスクを実行）
-const watchFile = (done) => {
-  gulp.watch('./src/scss/*.scss', sassCompile); // 第1引数：監視するファイルのパス　第2引数：コンパイル処理の変数
+const watchFiles = (done) => {
+  gulp.watch('./*.html', reloadBrowser);
+  gulp.watch('./css/*.css', reloadBrowser);
+  gulp.watch('./js/*/js',reloadBrowser );
+  gulp.watch('./scss/*.scss', sassCompile); // 第1引数：監視するファイルのパス　第2引数：コンパイル処理の変数
   done();
 };
 
 // タスク実行
 exports.default = gulp.series(
-  watchFile, sassCompile
+  watchFiles, sassCompile, reloadFile
 );
